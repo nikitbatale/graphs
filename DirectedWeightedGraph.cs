e616db617ce06a939e8a86089356057b9fe5c6c2
nikit <nikit.batale@gmail.com>
2013-12-01 15:18:06 +0530

    Changed AddEdge method to accept edge instead of two nodes in accordance with base class

--- DirectedWeightedGraph.cs
+++ DirectedWeightedGraph.cs
@@ -18,11 +18,11 @@ namespace Graphs {
         public bool AddEdge(Node<NodeType> fromNode, Node<NodeType> toNode, T3 weight) {
             if (!(Nodes.Contains(fromNode) && Nodes.Contains(toNode))) return false;
             DirectedWeightedEdge<NodeType, T3> dwe = new DirectedWeightedEdge<NodeType, T3>(fromNode, toNode, weight);
-            return Edges.Add(dwe);
+            return AddEdge(dwe);
         }
 
-        public override bool AddEdge(Node<NodeType> node1, Node<NodeType> node2) {
-            throw new NotImplementedException();
+        public override bool AddEdge(Edge<NodeType> edge) {
+            return Edges.Add(edge);
         }
     }
 }
