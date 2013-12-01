3b5b6cfb76e7b87f8a7a899c326aac1f2b5eb2e8
nikit <nikit.batale@gmail.com>
2013-12-02 01:04:08 +0530

    Added ToString, Equals and GetHashCode methods

--- Graph.cs
+++ Graph.cs
@@ -6,8 +6,8 @@ using System.Text;
 
 namespace Graphs {
     public abstract class Graph<NodeType> {
-        HashSet<Node<NodeType>> nodes;
-        HashSet<Edge<NodeType>> edges;
+        private HashSet<Node<NodeType>> nodes;
+        private HashSet<Edge<NodeType>> edges;
 
         public Graph(){
             this.nodes = new HashSet<Node<NodeType>>();
@@ -21,10 +21,44 @@ namespace Graphs {
         public HashSet<Edge<NodeType>> Edges {
             get { return edges; }
         }
-
+        
         public int Order() { return nodes.Count(); }
         public int Size() { return edges.Count(); }
 
         public abstract bool AddEdge(Edge<NodeType> edge);
+
+        public override string ToString() {
+            string ret = "";
+            if (nodes.Count == 0) ret += "No nodes added\n";
+            ret += "Nodes:\n";
+            foreach (Node<NodeType> node in nodes) ret += node.ToString() + "\n";
+            if (edges.Count == 0) ret += "No edges added\n";
+            ret += "Edges:\n";
+            foreach (Edge<NodeType> edge in edges) ret += edge.ToString() + "\n";
+            return ret;
+        }
+
+        public override bool Equals(object obj) {
+            if (this == obj) return true;
+            Graph<NodeType> comparisionObject = obj as Graph<NodeType>;
+            foreach (Node<NodeType> node in nodes)
+                if (!comparisionObject.Nodes.Contains(node)) {
+                    Console.WriteLine(node.ToString() + " x ");
+                    return false; }
+            foreach (Edge<NodeType> edge in edges)
+                if (!comparisionObject.Edges.Contains(edge)) {
+                    Console.WriteLine(edge.ToString() + " x ");
+                    return false; }
+            return true;
+        }
+
+        public override int GetHashCode() {
+            int hashCodeConstant = 17;
+            foreach (Node<NodeType> node in nodes) hashCodeConstant += node.GetHashCode();
+            foreach (Edge<NodeType> edge in edges) hashCodeConstant += edge.GetHashCode();
+            return base.GetHashCode();
+        }
+
+        
     }
 }
