9a631fc1a55959ba63061a85fda230d55b418de6
nikit <nikit.batale@gmail.com>
2013-12-03 00:53:44 +0530

    Concrete implementation of SubGraph for DirectedWeightedGraph

--- DirectedWeightedGraph.cs
+++ DirectedWeightedGraph.cs
@@ -26,6 +26,19 @@ namespace Graphs {
             return Edges.Add(edge);
         }
 
+        public override Graph<NodeType> SubGraph(params Edge<NodeType>[] subgraphEdges) {
+            foreach (Edge<NodeType> edge in subgraphEdges) 
+                if (!Edges.Contains(edge))
+                    throw new InvalidSubgraphEdgeException("Attempting to construct a subgraph from an edge that does not exist in the original graph.");
+            DirectedWeightedGraph<NodeType, T3> subGraph = new DirectedWeightedGraph<NodeType, T3>();
+            foreach (Edge<NodeType> edge in subgraphEdges) {
+                subGraph.AddNode(edge.EndNodes.Item1);
+                subGraph.AddNode(edge.EndNodes.Item2);
+                subGraph.AddEdge(edge);
+            }
+            return subGraph;
+        }
+
         public override bool Equals(object obj) {
             return base.Equals(obj);
         }
