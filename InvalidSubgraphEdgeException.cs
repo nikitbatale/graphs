d8206766edf6f550b8fad2e1d817046cd8ba365b
nikit <nikit.batale@gmail.com>
2013-12-03 00:55:12 +0530

    Exception to throw when there is an attempt to extract a subgraph having edges that the parent graph doesnt hve

---
+++ InvalidSubgraphEdgeException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Graphs {
+    class InvalidSubgraphEdgeException : Exception {
+        public InvalidSubgraphEdgeException(string message) : base(message){
+        }
+    }
+}
